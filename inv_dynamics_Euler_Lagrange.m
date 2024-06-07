daeb35e318913ca205b0f0740f65b74ae306c163
Ashutosh Mukherjee <ashutosh.mukherjeecpg@gmail.com>
2024-06-07 16:21:41 +0200

    cleaned the main file a little. Next step: making the code symbolic

--- inv_dynamics_Euler_Lagrange.m
+++ inv_dynamics_Euler_Lagrange.m
@@ -9,26 +9,26 @@
 % For Rotary Joint (Flag=1),
 % For Prismatic Joint (Flag=0)
 %**************************************************************************
-%current computation time = 10.3 minutes (618 seconds)
+% current computation time = 0.0948 seconds
 %% SETTINGS
 clc
-clear all
+clearvars
 close all
-format bank
+format short
 
 % GLOBAL VARIABLES
-global NJ NF DOF 
+global NJ
 
 NJ = 3;
 
-%type of joint
+% type of joint
 flag = zeros(NJ,1);
 for i = 1:NJ
     if i ~= 3
         flag(i) = 1;%revolute
     end
 end
-%link masses
+% link masses
 for i = 1:NJ
     if i == 1 || i == 2
     link_masses(i) = 1;
@@ -37,7 +37,7 @@ for i = 1:NJ
     end
 end
 
-%link widths
+% link widths
 w = zeros(NJ,1);
 
 %link lengths
@@ -48,13 +48,6 @@ end
 
 %height of links
 H = zeros(NJ,1);
-% for i = 1:NJ
-%     if i == 3
-%         H(i) = 0.1;
-%     elseif i == 4
-%         H(i) = 0.2;
-%     end
-% end
 
 %position of link i COM wrt to ith frame (right)
 for i = 1:NJ
@@ -101,56 +94,6 @@ A = zeros(4,4,NJ);% arm matrix
 % Calculate homogeneous tranforms of each frame 
 tic
 [A] = ArmMatrix(A,inputFileDHP);
-% 
-% % Calculate the tip to base tip to base concatenated matrix
-% tip_frame = 2; % frame at toe tip
-% base_frame = 0; % frame at base or pelvis
-% 
-% A1 = T_Concat(A,tip_frame,base_frame); % Here  
-
-% Plot the final concated matrix 
-% A1
-
-% % If you want to see the transformation martix of 8th frame
-% A(:,:,8);  
-
-% % If you want to calculate the matric of ankle tip to Knee (extension)
-% % See fig in the paper, knee extension belongs to frame 5
-% A1 = T_Concat(A,tip_frame,5);  
-% 
-% for kk = 1:length(impt_vars.sim_time(1:end-1))
-%     A1{kk}(1,4) = A1{kk}(1,4) + impt_vars.r_asis_x(kk);
-%     A1{kk}(2,4) = A1{kk}(2,4) + impt_vars.r_asis_y(kk);
-%     A1{kk}(3,4) = A1{kk}(3,4) + impt_vars.r_asis_z(kk);
-%     x_data_toe(kk) = A1{kk}(1,4);
-%     y_data_toe(kk) = A1{kk}(2,4);
-%     z_data_toe(kk) = A1{kk}(3,4);
-% end
-
-% figure(1)
-% hold on
-% aa = plot(impt_vars.sim_time,impt_vars.r_met_x,'*')
-% bb = plot(impt_vars.sim_time,x_data_toe)
-% legend([aa,bb],'Marker Data','MATLAB result in SP')
-% title('Right Toe (r-met marker) X - trajectory')
-% xlabel('Time (s)')
-% ylabel('X-coordinate of Toe Marker')
-% figure(2)
-% hold on
-% cc = plot(impt_vars.sim_time,impt_vars.r_met_y,'*')
-% dd = plot(impt_vars.sim_time,y_data_toe)
-% legend([cc,dd],'Marker Data','MATLAB result in SP')     
-% title('Right Toe (r-met marker) Y - trajectory')
-% xlabel('Time (s)')
-% ylabel('Y-coordinate of Toe Marker')
-% figure(3)
-% hold on
-% ee = plot(impt_vars.sim_time,impt_vars.l_met_z,'*')
-% ff = plot(impt_vars.sim_time,z_data_toe)
-% legend([ee,ff],'Marker Data','MATLAB result in SP')
-% title('Left Toe (l-met marker) Z - trajectory')
-% xlabel('Time (s)')
-% ylabel('Z-coordinate of Toe Marker')
 
 %inertia matrix
 J = inertia_matrix(Ixx,Iyy,Izz,Ixy,Ixz,Iyz,link_masses,r,NJ);
